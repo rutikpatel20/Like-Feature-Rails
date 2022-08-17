169e749221d9cc4c0d420dea9ef846be42389fee
rutikpatel20 <rutik.p@simformsolutions.com>
2022-08-17 15:20:49 +0530

    generate likes model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_17_094516) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_17_095006) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_17_094516) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "blog_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_likes_on_blog_id"
+    t.index ["user_id", "blog_id"], name: "index_likes_on_user_id_and_blog_id", unique: true
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_17_094516) do
   end
 
   add_foreign_key "blogs", "users"
+  add_foreign_key "likes", "blogs"
+  add_foreign_key "likes", "users"
 end
